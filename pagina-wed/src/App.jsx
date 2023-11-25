f96b0fc33d1748bc7dde92dbd53462c3de3820ae
karol cardona <kfcardonaaponte14@gmail.com>
2023-11-25 16:32:37 -0500

    segundo

--- pagina-wed/src/App.jsx
+++ pagina-wed/src/App.jsx
@@ -23,6 +23,17 @@ function App() {
   <header>
     <h1>Tarea para hoy</h1>
   </header>
+
+  <main>
+  <section id="about">
+    <p>No mires asi atras con ira, ni hacia adelante con miedo, sino alrededor con atencion, aquella hermosa magia libra mil batallas si luchas por tus sueños y te arriesgas. 
+      Acuerdate que la felicidad se logra  si crees en ti y las capacidades que puedes ofrecer a pesar de la oscuridad y los diversos problemas que nos sucedan lucha y nunca te rindas, ni te dejes vencer por ninguna adversidad que detenga nuestro maravilloso camino.</p>
+  </section>
+  <footer>
+    <p>© 2023 Nuestra Empresa. Todos los derechos reservados.</p>
+  </footer>
+</main>
+
   
 </div>
 
